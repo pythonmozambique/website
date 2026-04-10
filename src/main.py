77f8db96badd8d7cffe02d0dbfceaaf8d9a04fd5
onil.dc <onilmatavele@gmail.com>
2026-04-10 02:45:26 +0200

    Adicionei animações de scrool usando a biblioteca AOS

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from browser import alert
 from browser import document, html
 
 # --- NAVBAR ---
@@ -12,13 +13,16 @@ document <= navbar
 
 # --- HOME / HERO ---
 home_section = html.DIV(Id="home", Class="section")
+home_section.attrs['data-aos'] = 'fade-up'
 home_section <= html.H1("Bem-vindo à Comunidade Python Moçambique")
-home_section <= html.P("A nossa missão é conectar, educar e inspirar todos os entusiastas de Python em Moçambique.")
+home_section <= html.P(
+    "A nossa missão é conectar, educar e inspirar todos os entusiastas de Python em Moçambique.")
 home_section <= html.BUTTON("Junte-se à Comunidade", Id="join-btn")
 document <= home_section
 
 # --- SOBRE ---
 sobre_section = html.DIV(Id="sobre", Class="section")
+sobre_section.attrs['data-aos'] = 'fade-up'
 sobre_section <= html.H2("Sobre Nós")
 sobre_section <= html.P("Python Moçambique é uma comunidade aberta para todos: programadores iniciantes, estudantes, profissionais e educadores. "
                         "Promovemos workshops, encontros e projetos colaborativos para fortalecer o ecossistema Python local.")
@@ -26,6 +30,7 @@ document <= sobre_section
 
 # --- EVENTOS ---
 eventos_section = html.DIV(Id="eventos", Class="section")
+eventos_section.attrs['data-aos'] = 'fade-up'
 eventos_section <= html.H2("Próximos Eventos")
 eventos = [
     ("PyCon Maputo 2026", "15 de Março, Maputo"),
@@ -40,6 +45,7 @@ document <= eventos_section
 
 # --- RECURSOS ---
 recursos_section = html.DIV(Id="recursos", Class="section")
+recursos_section.attrs['data-aos'] = 'fade-up'
 recursos_section <= html.H2("Recursos")
 links_recursos = [
     ("Documentação Python", "https://docs.python.org/3/"),
@@ -54,6 +60,7 @@ document <= recursos_section
 
 # --- CONTACTO ---
 contacto_section = html.DIV(Id="contacto", Class="section")
+contacto_section.attrs['data-aos'] = 'fade-up'
 contacto_section <= html.H2("Contacto")
 contacto_section <= html.P("Email: info@pythonmozambique.org")
 contacto_section <= html.P("Twitter: @PythonMoz")
@@ -62,12 +69,15 @@ document <= contacto_section
 
 # --- RODAPÉ ---
 footer = html.DIV(Id="footer", Class="footer")
+footer.attrs['data-aos'] = 'fade-up'
 footer <= html.P("© 2026 Python Moçambique. Todos os direitos reservados.")
 document <= footer
 
 # --- EVENTOS INTERACTIVOS ---
+
+
 def join(ev):
     alert("Obrigado por se juntar à comunidade! Siga-nos nas redes sociais.")
 
-from browser import alert
+
 document["join-btn"].bind("click", join)
