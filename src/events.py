183a02363798315e596219d2095d1d512a5bbbf4
onil.dc <onilmatavele@gmail.com>
2026-04-14 00:19:35 +0200

    mexi na parte do events...adicionei arquivos com as imagens que acabei por usar nas cards, trabalhei mais com cards

--- src/events.py
+++ src/events.py
@@ -1,4 +1,5 @@
-from browser import html
+﻿from browser import html
+
 
 def create():
     sec = html.DIV(Id="eventos", Class="section")
@@ -6,43 +7,44 @@ def create():
 
     # --- SEÇÃO DE EVENTOS ---
     sec <= html.H2("Eventos e Colaborações")
-    
+
     eventos_lista = [
-        ("PyCon Maputo 2026", "15 de Março, Maputo", "images/pycon.jpg"),
+        ("PyCon Maputo 2026", "15 de Março, Maputo", "https://th.bing.com/th/id/OIP.yoPeL8kFPnlzxd1UBb6mIgHaG1?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3"),
         ("Hackathon Python", "10 de Junho, Maputo", "images/hackathon.jpg")
     ]
 
     ev_container = html.DIV(Class="events-container")
     for nome, data, img_url in eventos_lista:
         card = html.DIV(Class="event-card")
-        card <= html.IMG(src=img_url, alt=nome, Class="event-image")        
+        card <= html.IMG(src=img_url, alt=nome, Class="event-image")
         card <= html.H3(nome)
         card <= html.P(data)
         ev_container <= card
-    
+
     sec <= ev_container
 
     # --- SEÇÃO DE COMUNIDADES ---
     sec <= html.H2("Nossas Comunidades")
 
     comunidades_lista = [
-        ("Workshop Django", "30 de Abril, Online", "images/django.jpg"),
-        ("DjangoGirls Maputo", "TBC, Maputo", "assets/dg.jpeg"),
-        ("PyLadies Maputo", "TBC, Online", "images/pyladies_maputo.jpg"),
-        ("PyLadies Beira", "TBC, Beira", "images/pyladies_beira.jpg")
+        ("Workshop Django", "", "img/dg.jpeg"),
+        ("DjangoGirls Maputo", "", "img/dg.jpeg"),
+        ("PyLadies Maputo", "", "img/plm.jpeg"),
+        ("PyLadies Beira", "", "img/plb.jpeg")
     ]
 
-    com_container = html.DIV(Class="events-container") # Usa a mesma classe para manter o estilo
+    com_container = html.DIV(Class="events-container")
     for nome, info, img_url in comunidades_lista:
         card = html.DIV(Class="event-card")
-        card <= html.IMG(src=img_url, alt=nome, Class="event-image")        
+        card <= html.IMG(src=img_url, alt=nome, Class="event-image")
         card <= html.H3(nome)
         card <= html.P(info)
         com_container <= card
 
     sec <= com_container
-    
+
     return sec
+
 
 def bind(document):
     pass
